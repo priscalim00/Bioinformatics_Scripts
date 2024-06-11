7cbc70bb21148970cbcd4117b1613283d127e0ae
Prisca Lim <prisca@live.unc.edu>
2024-06-11 14:08:13 -0400

    Script for deduplication
    This script currently doesn't work. Committing this as a placeholder for now.

--- code/preprocessing/01_deduplication.bash
+++ code/preprocessing/01_deduplication.bash
@@ -1,20 +1,31 @@
 #!/bin/bash
 
+#SBATCH -p general
+#SBATCH -N 1
+#SBATCH --mem 64g
+#SBATCH -n 16
+#SBATCH -t 1-
+#SBATCH --mail-type=all
+#SBATCH --mail-user=prisca@live.unc.edu
+
 # Deduplication of reads is performed to remove reads that are exact matches. These duplicated reads are likely due to PCR artifacts 
 # hence are removed to reduce the required  downstream computing power and avoid artificially inflated coverage in those regions
 
-# Deduplication is done using htstream which can be installed using Bioconda: `conda create --name htstream htstream`. 
-# Ensure this installation is complete before running this step
+module load htstream
 
-conda activate htstream
-
-from glob import glob
+import glob
 import os
-      
+
+mkdir data/working/deduplicated
+
 for r1 in glob("data/raw/*_R1_*.fastq.gz"):
     r2 = r1.replace("R1", "R2")
     s = r1.split('/')[-1].replace("_L001_R1_001.fastq.gz", '')
     cmd = "hts_SuperDeduper -L data/working/deduplicated/" + s + "_stats.log -1 " + r1 + " -2 " + r2 + " | "
+
+
+
+
 
 
 
