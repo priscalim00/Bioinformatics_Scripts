bc1a3f316a130852c741df7031ab79ce0ebb2bd5
Prisca Lim <prisca@live.unc.edu>
2024-06-12 11:35:14 -0400

    Script to obtain raw sequences from Longleaf

---
+++ data/raw/obtain_raw.bash
@@ -0,0 +1,7 @@
+#/usr/bin/env bash
+
+#This script is used to obtain raw sequencing files from this patient cohort
+
+cd data/raw/
+
+cp -uf /proj/andermannlab/working_files/BMT_Novaseq/*.fastq.gz .
