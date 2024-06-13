bf29485caa473d8a539337ef8a5ae18e9b1615cb
Prisca Lim <prisca@live.unc.edu>
2024-06-13 11:34:33 -0400

    Script for MAG assembly to be tested

---
+++ code/assembly/00_assembly.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH -p general
+#SBATCH -N 1
+#SBATCH --mem 64g
+#SBATCH -n 16
+#SBATCH -t 2-
+#SBATCH --mail-type=all
+#SBATCH --mail-user=prisca@live.unc.edu
+
+# This script acts to assemble each sample into MAGs individually.
+
+# Input: Host-removed reads found in data/working/host_removed/ -> copied to data/processed/reads
+# Output: A number of output files will be deposited into data/processed/assembly/"$sample"
+#         The main file of interest is scaffolds.fasta which contains the assembled MAGs
+
+module purge #removes any loaded modules
+module load spades/4.0.0
+
+mkdir data/processed/reads 
+cp data/working/host_removed/*fastq.gz data/processed/reads/ 
+
+mkdir data/processed/assembly
+
+cd data/processed/reads
+
+for file in *R1.fastq.gz 
+do 
+	R1=$(ls $file)
+	R2=${R1//R1.fast1.gz/R2.fastq.gz} 
+	sample=$(ls $file | sed 's/_.*//')
+
+	spades.py --meta -k auto \
+	-1 "$R1" -2 "$R2" -o ../assembly/"$sample"
+done
