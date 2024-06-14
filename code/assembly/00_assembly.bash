eb99871218bfbf618b23e83fe0bd07423a01a3c5
Prisca Lim <prisca@live.unc.edu>
2024-06-14 08:30:26 -0400

    Increased resource request for assembly script

--- code/assembly/00_assembly.bash
+++ code/assembly/00_assembly.bash
@@ -2,8 +2,8 @@
 
 #SBATCH -p general
 #SBATCH -N 1
-#SBATCH --mem 64g
-#SBATCH -n 16
+#SBATCH --mem 128g
+#SBATCH -n 32
 #SBATCH -t 2-
 #SBATCH --mail-type=all
 #SBATCH --mail-user=prisca@live.unc.edu
@@ -38,6 +38,5 @@ do
 	R2=${R1//R1.fast1.gz/R2.fastq.gz} 
 	sample=$(ls $file | sed 's/_.*//')
 
-	spades.py --meta -k auto \
-	-1 "$R1" -2 "$R2" -o ../assembly/"$sample"
+	spades.py --meta -k auto -1 "$R1" -2 "$R2" -o ../assembly/"$sample"
 done
