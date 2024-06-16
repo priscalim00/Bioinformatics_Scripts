840089eaafe91a62534328e99c17d1e248c1040c
Prisca Lim <prisca@live.unc.edu>
2024-06-16 14:48:37 -0400

    Started assembly pipeline

--- code/analysis_driver.bash
+++ code/analysis_driver.bash
@@ -8,15 +8,23 @@ bash data/raw/obtain_raw.bash
 #Obtain human reference genome
 sbatch data/reference/obtain_reference.bash
 
-#Preprocessing step 00: Run fastqc and multiqc on raw sequencing files
+#Preprocessing:
+##Step 00: Run fastqc and multiqc on raw sequencing files
 sbatch code/preprocessing/00_fastqc.bash
 
-#Preprocessing step 01: Run hts_SuperDeduper on raw sequencing files
+##Step 01: Run hts_SuperDeduper on raw sequencing files
 sbatch code/preprocessing/01_deduplication.bash
 
-#Preprocessing step 02: Trimming low quality reads and adapter sequences from deduplicated files
+##Step 02: Trimming low quality reads and adapter sequences from deduplicated files
 sbatch code/preprocessing/02_trimming.bash
 
-#Preprocessing step 03: Removing host reads from trimmed files
+##Step 03: Removing host reads from trimmed files
 sbatch code/preprocessing/03_hostremoval.bash
+
+##Step 04: Generate fastqc and multiqc reports for processed reads
+sbatch code/preprocessing/04_finalqc.bash
+
+#Assembly:
+##Step 00: Running metaspades
+sbatch code/assembly/00_assembly.bash
 
