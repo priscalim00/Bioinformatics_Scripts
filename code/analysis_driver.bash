a1da8a4145fa45466bb4430a80d88a8ee5a33747
Prisca Lim <prisca@live.unc.edu>
2024-07-10 11:37:54 -0400

    Added Bracken post processing step

--- code/analysis_driver.bash
+++ code/analysis_driver.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This serves as the main analysis driver script for the project
+#This serves as the main analysis driver script for the project, needs to be edited HEAVILY pls don't use rn
 
 #Pull raw sequencing files from /proj/andermannlab
 bash data/raw/obtain_raw.bash
@@ -9,8 +9,9 @@ bash data/raw/obtain_raw.bash
 sbatch data/reference/obtain_reference.bash
 
 #Generate sample list
+mkdir -p data/summary
 cd data/raw/
-ls *R1_001.fastq.gz | sed 's/_.*//' > ../../all_samples.txt
+ls *R1_001.fastq.gz | sed 's/_.*//' > data/summary/all_samples.txt
 
 #Preprocessing:
 ##Step 00: Run fastqc and multiqc on raw sequencing files
@@ -27,6 +28,26 @@ sbatch code/preprocessing/03_hostremoval.bash
 
 ##Step 04: Generate fastqc and multiqc reports for processed reads
 sbatch code/preprocessing/04_finalqc.bash
+
+#Annotating processed reads with Kraken2 & Bracken
+##Step 01: Run Kraken2, Bracken & KrakenTools
+for sample in $(cat all_samples.txt); do sbatch code/annotation/00_krakenbracken.bash
+
+##Step 02: Combine Bracken outputs into one file
+module purge
+module load kraken/2.1.2 python/2.7.12
+python "$conda_envs"/kraken2bracken/bin/combine_bracken_outputs.py \
+	--files "$bracken_dir"/species/*_bracken_species.txt \
+	-o "$bracken_dir"/species/all_bracken_species.txt
+
+python "$conda_envs"/kraken2bracken/bin/combine_bracken_outputs.py \
+        --files "$bracken_dir"/genus/*_bracken_genus.txt \
+        -o "$bracken_dir"/genus/all_bracken_genus.txt
+ 
+python "$conda_envs"/kraken2bracken/bin/combine_bracken_outputs.py \
+        --files "$bracken_dir"/order/*_bracken_order.txt \
+        -o "$bracken_dir"/order/all_bracken_order.txt
+module purge
 
 #Assembly:
 ##Step 00: Running metaspades
